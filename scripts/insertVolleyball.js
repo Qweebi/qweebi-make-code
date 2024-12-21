42000d23996de7f47182b7ce3ad4181c93ae8dde
Varun Barad <varun@qweebi.com>
2024-12-21 13:00:18 +0530

    Convert volleyball script to multi-line string.

--- scripts/insertVolleyball.js
+++ scripts/insertVolleyball.js
@@ -1,3 +1,11 @@
+const stringToInsert = '<script>' +
+  'window.addEventListener("message", (event) => {' +
+  '  if (!!window.parent && !!event.data.codeCommand) {' +
+  '    window.parent.postMessage(event.data, "*");' +
+  '  }' +
+  '});' +
+  '</script>';
+
 let input = '';
 
 // Listen for data events on stdin
@@ -8,6 +16,6 @@ process.stdin.on('data', (chunk) => {
 // Listen for the end of the input stream
 process.stdin.on('end', () => {
   const lines = input.split('\n');
-  lines.splice(lines.length - 4, 0, '<script>window.addEventListener(\'message\', (event) => { if (!!window.parent && !!event.data.codeCommand) { window.parent.postMessage(event.data, \'*\'); } });</script>');
+  lines.splice(lines.length - 4, 0, stringToInsert);
   console.log(lines.join('\n'));
 });
